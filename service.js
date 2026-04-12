e5961ae2a06a5d53272af9789bac95838c9e3055
ainsleylacroix123 <ainsleylacroix123@gmail.com>
2026-04-12 07:55:29 -0400

    Add files via upload

---
+++ service.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = "recipe-app-v2"; 
+
+self.addEventListener("install", event => {
+  self.skipWaiting(); 
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
+});
