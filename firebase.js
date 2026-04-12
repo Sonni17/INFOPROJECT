270c56d4c404edefc5f99fd274654d2b9f8044a9
ainsley lacroix <ainsleylacroix123@gmail.com>
2026-04-12 18:58:41 -0400

    new changes firebase inclusive

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBbW4Rwm6lJxc0u5OI5K887zAQbIwcYF9A",
+  authDomain: "info-1601-project-fdc77.firebaseapp.com",
+  projectId: "info-1601-project-fdc77",
+  storageBucket: "info-1601-project-fdc77.firebasestorage.app",
+  messagingSenderId: "188792371451",
+  appId: "1:188792371451:web:95e6e74e1f7c36561f585e",
+  measurementId: "G-CG6E6V0Q5K"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// export so other files can use it
+export { app, db };
